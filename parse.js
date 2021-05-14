58e9a27010fc716ce42750f337b9c691fb460038
gamekingv <793470218@qq.com>
2021-05-14 19:21:16 +0800

    Initialize

--- parse.js
+++ parse.js
@@ -7,5 +7,6 @@ const {
 (async () => {
   const manifest = JSON.parse(await fs.readFile('ehsyringe.chrome/manifest.json'));
   manifest.update_url = `https://github.com/${repository}/releases/latest/download/update.xml`;
+  process.env['EHSYRINGE_VERSION'] = manifest.version;
   await fs.writeFile('ehsyringe.chrome/manifest.json', JSON.stringify(manifest, null, 2));
 })();
